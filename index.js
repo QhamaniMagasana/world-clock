cfe0bbc7cc313c9777a34b3905862474a8f3c0c4
Qhamani Magasana <qhamanimagasana@gmail.com>
2026-05-11 20:17:50 +0200

    added functionaity to select

--- index.js
+++ index.js
@@ -37,5 +37,28 @@ jamaica.format("h:mm:ss [<small>]A[</small>]"
 );
 
 }
+
+
+function displayTime(event){
+    
+   let cityTimeZone = event.target.value;
+   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+   let cityTime = moment().tz(cityTimeZone);
+   let citiesElement = document.querySelector("#cities");
+   citiesElement.innerHTML = `  
+   <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
+        </div>
+    </div>`;
+}
+
+
 changeTime();
 setInterval(changeTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", displayTime);
